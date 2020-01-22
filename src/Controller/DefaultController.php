9ea50978286f7ff6b88d9064347e7977e31f2316
Liiisey <delalande.lisa@outlook.fr>
2020-01-22 14:49:03 +0100

    Création de l'inscription (make:registration)

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -16,30 +16,27 @@ class DefaultController extends AbstractController
 
     public function index()
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
-
         //Rechercher les données en base de données
         $projects = $this->getDoctrine()->getRepository(Project::class)->findAll();
         $categories = $this->getDoctrine()->getRepository(Category::class)->findAll();
 
+        //Envoyer les données à la vue
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+            'projects' => $projects,
+            'categories' => $categories,
+            'projectForm' => $projectForm->createView()
+        ]);
+
         //Afficher l'adresse de l'utilisateur quand il est connecté dans le formulaire
         $project = new Project();
         if ($this->getUser()) {
-            $project->setEmail($this->getUser()->getEmail());
+            $user->setEmail($this->getUser()->getEmail());
         }
 
         //Formulaire des projets
         $projectForm = $this->createForm(ProjectType::class, $project, [
             'action' => $this->generateUrl('project_new')
-        ]);
-
-        //Envoyer les données à la vue
-        return $this->render('default/index.html.twig', [
-            'projects' => $projects,
-            'categories' => $categories,
-            'projectForm' => $projectForm->createView()
         ]);
     }
 
